e36d99ca2fe64523e866188f99c9d2acac9a9d4a
rdl123 <ramirachid316@gmail.com>
2021-04-05 10:48:13 +0100

    added spinner

--- web/src/Demo/Charts/Nvd3Chart/index.js
+++ web/src/Demo/Charts/Nvd3Chart/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Row, Col, Card, } from 'react-bootstrap';
+import {Row, Col, Card,Spinner } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import ReactSpinner from 'react-bootstrap-spinner'
+
 
 import Aux from "../../../hoc/_Aux/index";
 import LineChart from "./LineChart";
@@ -37,8 +37,8 @@ class Nvd3Chart extends React.Component {
                         <Card>
                             <Card.Header>
                                 <Card.Title as="h5">Nombre de defibrillateur par ville</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
+                            </Card.Header >
+                            <Card.Body className="text-center">
                                 <BarDiscreteChart data = {this.props.Stats_Prov}/>
                             </Card.Body>
                         </Card>
@@ -49,12 +49,13 @@ class Nvd3Chart extends React.Component {
                             <Card.Header>
                                 <Card.Title as="h5">Nombre de defibrillateurs par statut</Card.Title>
                             </Card.Header>
-                            {this.props.Stats !== [] ?(
+                         
                                 <Card.Body className="text-center">
-                                   <PieDonutChart data = {this.props.Stats} />
+                                    <PieDonutChart data = {this.props.Stats} />
+                                    
+                                 
                                 </Card.Body>
-                            ): <ReactSpinner type="border" color="primary" size="5" />}
-                            
+                          
                         </Card>
                     </Col>
                 </Row>
