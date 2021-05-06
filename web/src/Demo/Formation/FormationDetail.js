21e55a171a85ac088e6d1a0d79eeb1cb59a8250c
Ali Soualy <alisoualy01@gmail.com>
2021-05-06 00:15:19 +0000

    added web changes formation

--- web/src/Demo/Formation/FormationDetail.js
+++ web/src/Demo/Formation/FormationDetail.js
@@ -36,52 +36,111 @@ function FormationDetail() {
     return (
         <div>
           {Formation ? 
-          (<div style = {{ display:'flex',flexDirection:'row', justifyContent: 'space-between'}}>
-               <img src={Formation.image} width ="150px" height = "200px"/>
-               <div style ={{margin:20, marginTop : 0}} >
-                   <span > {Formation.nom}</span>
-                   {Formation.desription}
+          (<div class="row">
+               <img class="col-sm4" src={Formation.image} width ="200" height = "250"/>
+               <div class="col-sm" style ={{margin:20, marginTop : 0}} >
+                    <h3 class="font-weight-bold" > {Formation.nom}</h3>
+                    <Card style={{padding:10}}>
+                        <p class="font-italic" >{Formation.desription}</p>
+                    </Card>
                 </div>
-                <div>
-                    <div>{Formation.nbr_inscrit}</div>
-                    <div>test</div>
-                    <div>{Formation.date_debut}</div>
+                <div class="col-sm4">
+                        <Card>
+                            <Card.Body className='border-bottom'>
+                                <div className="row d-flex align-items-center">
+                                    <div className="col-auto">
+                                        <i className="feather icon-user-check f-30 text-c-green"/>
+                                    </div>
+                                    <div className="col">
+                                        <h3 className="f-w-300">{Formation.nbr_inscrit} / {Formation.nbr_max}</h3>
+                                        <span className="d-block text-uppercase">Nombre d'inscription (Individu)</span>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                            <Card.Body className='border-bottom'>
+                                <div className="row d-flex align-items-center">
+                                    <div className="col-auto">
+                                        <i className="feather icon-briefcase f-30 text-c-blue"/>
+                                    </div>
+                                    <div className="col">
+                                        <h3 className="f-w-300">26</h3>
+                                        <span className="d-block text-uppercase">Nombre d'inscription (Entreprise)</span>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                            <Card.Body>
+                                <div className="row d-flex align-items-center">
+                                    <div className="col">
+                                    <Button variant="primary" onClick = {Modify}>
+                                        Modifier
+                                    </Button>
+                                    </div>
+                                    <div className="col">
+                                    <Button variant="primary" onClick = {Delete}>
+                                            supprimer
+                                    </Button>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
                 </div>
-                <Button variant="primary" onClick = {Modify}>
-                         Modifier
-                </Button>
-                <Button variant="primary" onClick = {Delete}>
-                         supprimer
-                </Button>
+                
           </div>
           ) 
           : null}
-           <Table striped responsive>
-                <thead>
-                        <tr>
-                            <th>Nom</th>
-                            <th>prenom</th>
-                            <th>email</th>
-                            <th>Formation</th>
-                            <th>date d'inscription</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {typeof inscrits !== "undefined" &&  inscrits.map(item => 
-                        <tr key = {item.id}>
-                        <td>{item.user.firstName}</td>
-                        <td>{item.user.lastName}</td>
-                        <td>{item.user.email}</td>
-                        <td>{item.formation.nom}</td>
-                        <td>{item.date_inscription}</td>
-                    
-                    
-                    </tr>)
-                        }
-                            
-             </tbody>
-             </Table>    
+          <Tabs defaultActiveKey="home">
+                                         <Tab eventKey="home" title="Individus">
+                                                    <Table striped responsive>
+                                                            <thead>
+                                                                <tr>
+                                                                    <th>Nom</th>
+                                                                    <th>prenom</th>
+                                                                    <th>email</th>
+                                                                    <th>Formation</th>
+                                                                    <th>date d'inscription</th>
+                                                                    <th></th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                {typeof inscrits !== "undefined" &&  inscrits.map(item => 
+                                                                <tr key = {item.id}>
+                                                                    <td>{item.user.firstName}</td>
+                                                                    <td>{item.user.lastName}</td>
+                                                                    <td>{item.user.email}</td>
+                                                                    <td>{item.formation.nom}</td>
+                                                                    <td>{item.date_inscription}</td>
+                                                                </tr>)
+                                                                }
+                                                            </tbody>
+                                                    </Table>    
+                                            </Tab>
+                                            <Tab eventKey="profile" title="Entreprise">
+                                                    <Table striped responsive>
+                                                            <thead>
+                                                                <tr>
+                                                                    <th>Nom</th>
+                                                                    <th>Entreprise</th>
+                                                                    <th>Telephone</th>
+                                                                    <th>Formation</th>
+                                                                    <th>date d'inscription</th>
+                                                                    <th></th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                {typeof inscrits !== "undefined" &&  inscrits.map(item => 
+                                                                <tr key = {item.id}>
+                                                                    <td>{item.user.firstName}</td>
+                                                                    <td>{item.user.lastName}</td>
+                                                                    <td>{item.user.email}</td>
+                                                                    <td>{item.formation.nom}</td>
+                                                                    <td>{item.date_inscription}</td>
+                                                                </tr>)
+                                                                }
+                                                            </tbody>
+                                                    </Table>    
+                                            </Tab>     
+                                         </Tabs>
+           
         </div>
     )
 }
