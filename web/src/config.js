b82c1d7cac27721c75b95fbb2f5f73e6d88c484b
rdl123 <ramirachid316@gmail.com>
2021-06-15 17:55:04 +0100

    added null

--- web/src/config.js
+++ web/src/config.js
@@ -1,6 +1,6 @@
 export default {
     defaultPath: '/auth/signin-1',
-    basename: '/', // only at build time to set, like /datta-able
+    basename: '/datta-able', // only at build time to set, like /datta-able
     layout: 'vertical', // vertical, horizontal (not available in lite version)
     preLayout: null, // (not available in lite version)
     collapseMenu: false, // mini-menu
